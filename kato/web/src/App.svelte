668d04f9026aa6b7cccb51076084a24bd1df3a3b
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-01-13 21:26:01 +0000

    tag store

--- kato/web/src/App.svelte
+++ kato/web/src/App.svelte
@@ -1,16 +1,23 @@
 <script>
-  import TagSearch from './components/TagSearch.svelte';
-  import Note from './components/Note.svelte';
+  import TagSearch from "./components/TagSearch.svelte";
+  import Note from "./components/Note.svelte";
+  import { tags } from "./store";
 </script>
 
 <main>
   <h1>Kato Web</h1>
 
   <div class="card">
+    <ul>
+      {#each $tags as tag}
+        <li>{tag.label}</li>
+      {/each}
+    </ul>
     <Note />
     <TagSearch />
-  </div>
 
+    
+  </div>
 </main>
 
 <style>
--- kato/web/src/components/TagSearch.svelte
+++ kato/web/src/components/TagSearch.svelte
@@ -1,24 +1,32 @@
 <script>
-    import Select from 'svelte-select';
-
-    let filterText = '';
+    import Select from "svelte-select";
+    import { tags } from "../store";
+    let filterText = "";
 
     let value = null;
 
-    let items = [
-      
-    ];
-
-    function handleFilter(e) {        
-        if (value?.find(i => i.label.toLowerCase().includes(filterText.toLowerCase()))) return;
+    function handleFilter(e) {
+        if (
+            value?.find((i) =>
+                i.label.toLowerCase().includes(filterText.toLowerCase()),
+            )
+        )
+            return;
         if (e.detail.length === 0 && filterText.length > 0) {
-            const prev = items.filter((i) => !i.created);
-            items = [...prev, { value: filterText, label: filterText, created: true }];
+            const prev = $tags.filter((i) => !i.created);
+            $tags = [
+                ...prev,
+                { value: filterText, label: filterText, created: true },
+            ];
         }
     }
-    
+
     function handleChange(e) {
-        items = items.map((i) => {
+        console.log(e);
+        $tags = $tags.map((i) => {
+            if (i.created) {
+                //CREATE
+            }
             delete i.created;
             return i;
         });
@@ -26,9 +34,17 @@
 </script>
 
 <div class="tagSearch">
-    <Select on:change={handleChange} multiple on:filter={handleFilter} bind:filterText bind:value {items}>
+    <Select
+        on:change={handleChange}
+        multiple
+        on:filter={handleFilter}
+        bind:filterText
+        bind:value
+        items={$tags}
+        on:create
+    >
         <div slot="item" let:item>
-            {item.created ? 'Add new: ' : ''}
+            {item.created ? "Add new: " : ""}
             {item.label}
         </div>
     </Select>
