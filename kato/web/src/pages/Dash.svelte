3f4abff3ca967f0c1f078403b28bdbaef80b567a
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-01-18 19:34:22 +0000

    dash view with latest added notes

--- kato/web/src/pages/Dash.svelte
+++ kato/web/src/pages/Dash.svelte
@@ -1,1 +1,15 @@
-<h1>Dash</h1>
+<script>
+  import { KATO_API_URL } from "../const";
+
+  let notePromise = fetch(`${KATO_API_URL}/notes?latest=true`).then((resp) =>
+    resp.json()
+  );
+</script>
+
+{#await notePromise then notes}
+  <ul>
+    {#each notes as note}
+      <li>{note.title} - {new Date(note.created_at).toLocaleDateString()}</li>
+    {/each}
+  </ul>
+{/await}
