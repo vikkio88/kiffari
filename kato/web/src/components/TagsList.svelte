92ee10b62bfc527e06fb805c5ee8787a17a67281
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-01-19 20:05:57 +0000

    improving tag list

--- kato/web/src/components/TagsList.svelte
+++ kato/web/src/components/TagsList.svelte
@@ -3,9 +3,33 @@
 </script>
 
 {#if tags.length < 1}
-  <h3>No tags...</h3>
+  <strong>No tags...</strong>
 {:else}
-  {#each tags as tag}
-    <h3>{tag.label}</h3>
-  {/each}
+  <div class="list">
+    {#each tags as tag}
+      <div class="tag">{tag.label}</div>
+    {/each}
+  </div>
 {/if}
+
+<style>
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5em;
+    padding: 2em;
+  }
+  .tag {
+    border: 2px white solid;
+    border-radius: 10px;
+    padding: 0.5em;
+    font-size: small;
+    font-weight: bold;
+  }
+
+  .tag:hover {
+    border-color: #646cff;
+    color: #747bff;
+    cursor: pointer;
+  }
+</style>
