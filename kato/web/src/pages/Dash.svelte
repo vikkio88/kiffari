6c1a69451604821721fc3d782fdbfedecc1905a0
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-01-20 14:43:33 +0000

    using note list

--- kato/web/src/pages/Dash.svelte
+++ kato/web/src/pages/Dash.svelte
@@ -1,16 +1,13 @@
 <script>
   import { Link } from "svelte-routing";
-import { KATO_API_URL } from "../const";
+  import { KATO_API_URL } from "../const";
+  import NoteList from "../components/NoteList.svelte";
 
   let notePromise = fetch(`${KATO_API_URL}/notes?latest=true`).then((resp) =>
-    resp.json()
+    resp.json(),
   );
 </script>
 
 {#await notePromise then notes}
-  <ul>
-    {#each notes as note}
-      <li> <Link to={`notes/${note.id}`}>{note.title}</Link> - {new Date(note.created_at).toLocaleDateString()}</li>
-    {/each}
-  </ul>
+  <NoteList {notes} />
 {/await}
