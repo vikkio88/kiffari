3d363bb97770c0408f2c91e7f418a0b3ee4ca7ce
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-02-12 20:13:58 +0000

    removed timeago

--- kato/web/src/main.js
+++ kato/web/src/main.js
@@ -1,9 +1,6 @@
 import './app.css';
 
 import App from './App.svelte';
-import TimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en";
-TimeAgo.addDefaultLocale(en);
 
 const app = new App({
   target: document.getElementById('app'),
