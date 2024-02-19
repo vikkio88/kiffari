321a3029a9953e992d631fa2620b4d4db0b380df
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-02-19 19:51:39 +0000

    implementing tabs for notes tags

--- kato/web/src/pages/Search.svelte
+++ kato/web/src/pages/Search.svelte
@@ -2,11 +2,23 @@
   import TagsList from "../components/TagsList.svelte";
   import Spinner from "../components/shared/Spinner.svelte";
   import { protectedRoute } from "../libs/routes";
-  import { filterTags, parseOrThrow, catchLogout } from "../libs/api";
+  import {
+    filterTags,
+    parseOrThrow,
+    catchLogout,
+    filterNotes,
+  } from "../libs/api";
+  import NoteList from "../components/NoteList.svelte";
   protectedRoute();
 
+  const TABS = {
+    TAGS: "tags",
+    NOTES: "notes",
+  };
+
+  let searchBar;
   let timer;
-  let tags = [];
+  let results = [];
   let controller = null;
   let searchPromise = null;
 
@@ -14,27 +26,49 @@
     clearTimeout(timer);
     timer = setTimeout(() => {
       if (v.length <= 2) {
-        tags = [];
+        results = [];
         return;
       }
 
       if (controller) controller.abort();
       controller = new AbortController();
 
-      searchPromise = filterTags(v, controller)
+      let search = activeTab === TABS.TAGS ? filterTags : filterNotes;
+
+      searchPromise = search(v, controller)
         .then(parseOrThrow)
         .catch(catchLogout);
     }, 500);
   };
+
+  let activeTab = TABS.TAGS;
+  function setActiveTab(tab) {
+    results = [];
+    searchPromise = null;
+    searchBar.value = "";
+    searchBar.focus();
+    activeTab = tab;
+  }
 </script>
 
 <h2>Search</h2>
+<div class="tabs">
+  <button
+    class:active={activeTab === TABS.TAGS}
+    on:click={() => setActiveTab(TABS.TAGS)}>Tags</button
+  >
+  <button
+    class:active={activeTab === TABS.NOTES}
+    on:click={() => setActiveTab(TABS.NOTES)}>Notes</button
+  >
+</div>
 <div class="tagSearch">
   <input
+    bind:this={searchBar}
     type="text"
     class="searchBar"
     on:keyup={({ currentTarget: { value } }) => debounce(value)}
-    placeholder="Tag..."
+    placeholder={activeTab === TABS.TAGS ? "Tag..." : "Note title, body...."}
   />
 </div>
 
@@ -42,11 +76,15 @@
   {#if searchPromise}
     {#await searchPromise}
       <Spinner />
-    {:then tags}
-      <TagsList big {tags} />
+    {:then results}
+      {#if activeTab === TABS.TAGS}
+        <TagsList big tags={results} />
+      {:else}
+        <NoteList notes={results} />
+      {/if}
     {/await}
   {:else}
-    <strong>Here there will be tags...</strong>
+    <strong>Here there will be search results...</strong>
   {/if}
 </div>
 
@@ -76,5 +114,24 @@
 
   strong {
     color: #a3a3a3;
+  }
+
+  div.tabs {
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  div.tabs > button {
+    border-radius: 0px;
+    margin: 0;
+  }
+  div.tabs > button.active {
+    background-color: black;
+    color: #646cff;
+  }
+  div.tabs > button.active:focus,
+  div.tabs > button.active:focus-visible {
+    outline: none;
   }
 </style>
