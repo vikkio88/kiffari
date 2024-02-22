7747d50b341ab43d67b7a9c563862dfa8ea7b5da
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-02-22 20:27:20 +0000

    moved project out of subdir

--- kato/scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-rm -rf kato_old
-tar xvf bin.tar.gz
-mv bin kato_new
-mv kato kato_old
-mv kato_new kato
-rm bin.tar.gz
---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+rm -rf kiffari_old
+tar xvf bin.tar.gz
+mv bin kiffari_new
+mv kiffari kiffari_old
+mv kiffari_new kiffari
+rm bin.tar.gz
