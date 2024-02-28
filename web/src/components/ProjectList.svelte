3224e0f51236886efa86cb3316f78018fc3919e4
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-02-28 19:44:08 +0000

    adding project list

---
+++ web/src/components/ProjectList.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { navigate } from "svelte-routing";
+
+  export let projects = [];
+</script>
+
+{#if projects.length < 1}
+  <h3>No notes... 🤷</h3>
+{:else}
+  <div class="projects">
+    {#each projects as project}
+      <div class="project">
+        <h2>{project.name}</h2>
+        <h3>{project.description}</h3>
+        <button on:click={() => navigate(`/projects/${project.id}`)}>➡️</button>
+      </div>
+    {/each}
+  </div>
+{/if}
+
+<style>
+  .projects {
+    display: grid;
+    grid-template-columns: auto auto;
+  }
+</style>
---
+++ web/src/pages/KiffariDash.svelte
@@ -0,0 +1,15 @@
+<script>
+  import ProjectList from "../components/ProjectList.svelte";
+  import Spinner from "../components/shared/Spinner.svelte";
+  import { getProjects } from "../libs/api";
+  import { protectedRoute } from "../libs/routes";
+  protectedRoute();
+  let projectsPromise = getProjects();
+</script>
+
+{#await projectsPromise}
+  <Spinner />
+{:then projects}
+  <h2>Projects</h2>
+  <ProjectList {projects} />
+{/await}
