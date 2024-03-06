1d360ac13453f239fdb00a86c572356a5430c933
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-03-06 21:17:30 +0000

    started working on copy text on click

--- web/src/components/renderers/Markdown.svelte
+++ web/src/components/renderers/Markdown.svelte
@@ -2,10 +2,31 @@
     import SvelteMarkdown from "svelte-markdown";
 
     export let body = "";
+    function copyText(e) {
+        navigator.clipboard.writeText(e.target.innerText);
+        //TODO: maybe add some sort of tooltip that tells you that the text is copie
+        //<span class="tooltiptext">Tooltip text</span> like in
+        //https://www.w3schools.com/css/tryit.asp?filename=trycss_tooltip_arrow_bottom
+    }
+
+    function handleCopyPaste(e) {
+        const { checked } = e.currentTarget;
+
+        console.log(checked);
+        const snippets = document.querySelectorAll("code");
+        if (checked) {
+            snippets.forEach((s) => s.addEventListener("click", copyText));
+        } else {
+            snippets.forEach((s) => s.removeEventListener("click", copyText));
+        }
+    }
 </script>
 
 <div class="mdBody">
     <SvelteMarkdown source={body} />
+</div>
+<div>
+    <input type="checkbox" on:change={handleCopyPaste} />
 </div>
 
 <style>
