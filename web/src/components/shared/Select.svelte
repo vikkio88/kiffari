965c2ebff6ec64cbc41b2c2380dff1f1e171df9e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-03-06 19:44:25 +0000

    adding select component

---
+++ web/src/components/shared/Select.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let selected = null;
+  export let options = [];
+</script>
+
+<select on:change={(e) => (selected = e.currentTarget.value)}>
+  {#each options as option}
+    <option
+      selected={(option.value ?? option) === selected}
+      value={option.value ?? option}
+    >
+      {option.label ?? option}
+    </option>
+  {/each}
+</select>
+
+
+<style>
+    select {
+        padding: .8rem;
+    }
+</style>
