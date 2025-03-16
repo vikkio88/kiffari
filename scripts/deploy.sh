309ec1ce79009b9a65196a9ec54c96a141c5ba5f
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-03-16 17:12:55 +0000

    restarting site

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,3 +4,4 @@ mv bin kiffari_new
 mv kiffari kiffari_old
 mv kiffari_new kiffari
 rm bin.tar.gz
+curl -X POST --basic --user "APIKEY account=ACCOUNT:" https://api.alwaysdata.com/v1/site/SITEID/restart/
